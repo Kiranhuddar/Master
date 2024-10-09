8709e15703cac1811aa3a1860d0526c3ec3531d0
unknown <huddarkiran@gmail.com>
2024-10-09 19:41:02 +0530

    First day project is completed

---
+++ pythonProject/developer_day1.py
@@ -0,0 +1,25 @@
+# print("Hello world\nHello world\nHello world")
+# print("Kiran "+"Huddar")
+#
+# name = input("What is your name?")
+# print("Hello"+" "+ name)
+
+# name ="kiran"
+# print(name)
+#
+# print(len(input("what us your name")))
+
+# user_name = input("Enter name :")
+# lenght = len(user_name)
+# print(lenght)
+#
+
+#project 1
+print("Welcome to the kiran Brand")
+city_name = (input("where are you from ?"))
+pet_name = (input("What is your favorate pet name ?"))
+print("so your brand name will be " + str(city_name)+" " + str(pet_name) )
+
+
+
+
